a597086a44a32faa812a511b73b8693bb537a717
Guilherme Augusto <guilherme.augusto23@hotmail.com>
2021-10-17 15:05:17 -0300

    Corrigindo erro com firebase

--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -1,7 +1,8 @@
 import firebase from 'firebase/compat/app';
 
-import 'firebase/auth';
-import 'firebase/database';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -11,9 +12,9 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
-  };
+};
 
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const database = firebase.database();
+const auth = firebase.auth();
+const database = firebase.database();

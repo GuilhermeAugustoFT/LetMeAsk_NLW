db0e78e8c42770c489eb55860ccc7dfa5aac48c5
Guilherme Augusto <guilherme.augusto23@hotmail.com>
2021-10-18 17:16:14 -0300

    Criação da página de rooms, push e get de novas perguntas no database do firebase

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
@@ -6,6 +6,7 @@ import { NewRoom } from "./pages/NewRoom";
 import { AuthContextProvider } from './contexts/AuthContext'
 
 import './styles/global.scss';
+import { Room } from './pages/Room';
 
 
 
@@ -16,8 +17,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider >
-        <Route path="/" exact component={Home}/>
-        <Route path="/rooms/new" component={NewRoom}/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/rooms/new" component={NewRoom}/>
+          <Route path="/rooms/:id" component={Room}/>
+        </Switch>
       </AuthContextProvider>
     </ BrowserRouter>
   );
---
+++ letmeask/src/components/RoomCode.tsx
@@ -0,0 +1,23 @@
+import copyImg from '../assets/images/copy.svg';
+
+import '../styles/roomCode.scss'
+
+type RoomCodeProps = {
+    code: string;
+}
+
+export function RoomCode(props: RoomCodeProps){
+
+    function copyRoomCodeToClipBoard() {
+        navigator.clipboard.writeText(props.code);
+    }
+
+    return (
+        <button className="room-code" onClick={copyRoomCodeToClipBoard}>
+            <div>
+                <img src={copyImg} alt="Copiar código da sala" />
+            </div>
+            <span>Sala #{props.code}</span>
+        </button>
+    );
+}
